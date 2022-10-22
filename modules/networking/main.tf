6c1be50302faa8e763a25a342c16665104cd4f7d
brooks <brooks.houck@gmail.com>
2022-10-22 15:22:03 +0000

    added security group for rds

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -7,4 +7,20 @@ module "vpc" {
   public_subnets               = var.public_sn
   create_igw                   = true
   single_nat_gateway           = true
+  create_database_subnet_group = true
+}
+
+resource "aws_security_group" "postgres_db_sg" {
+    name = "postgresql_sg"
+    description = "Security Group for PostgreSQL DB"
+    vpc_id = module.vpc.vpc_id 
+
+    ingress {
+        from_port = 5432
+        to_port   = 5432
+        protocol  = "tcp"
+        ##update to new subnet group cidr when created
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
 }
