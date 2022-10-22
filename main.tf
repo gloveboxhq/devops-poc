d61d1adb2c835915869e95304a05fae447d53324
brooks <brooks.houck@gmail.com>
2022-10-22 19:09:22 +0000

    updated modules

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "networking" {
   source = "./modules/networking"
-  azs = module.data.az_names.names 
+  #azs = module.data.az_names.names 
 }
 
 module "data" {
@@ -11,17 +11,23 @@ module "iam" {
     source = "./modules/iam"
     analyst_policy = module.data.rds_analyst_policy.arn
     admin_policy = module.data.admin_policy.arn
+    read-replica-arn = module.database.rds_read_replica.arn
 }
 
-/*
+
 module "database" {
     source = "./modules/database"
     kms_key = module.encryption.rds-key.arn
     rds_sg = module.networking.rds-sg.id
+    rds-secret = module.encryption.rds-password
+    rds-root-user = module.data.rdsrootuser.id
+
 }
-*/
+
 
 module "encryption" {
     source = "./modules/encryption"
     rds_supersecretpassword = module.data.rdspassword.result
+    secret-id = module.data.random_integer.result
+
 }
