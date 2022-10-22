0b93aca051ac48c8264a92e3f0abbd1c1a90e1c4
brooks <brooks.houck@gmail.com>
2022-10-22 15:44:09 +0000

    added output for password

--- modules/encryption/outputs.tf
+++ modules/encryption/outputs.tf
@@ -1,3 +1,6 @@
 output "rds-key" {
     value = aws_kms_key.rds_encryption_key
 }
+output "rds-password" {
+  value = jsonencode(data.aws_secretsmanager_secret_version.rds-password.secret_string)
+}
