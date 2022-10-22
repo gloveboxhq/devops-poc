00adec23f0772dac28bafb06bc9b59eb1dbc2a41
brooks <brooks.houck@gmail.com>
2022-10-22 15:19:12 +0000

    removed db module call for testing; added encryption module

--- main.tf
+++ main.tf
@@ -12,3 +12,16 @@ module "iam" {
     analyst_policy = module.data.rds_analyst_policy.arn
     admin_policy = module.data.admin_policy.arn
 }
+
+/*
+module "database" {
+    source = "./modules/database"
+    kms_key = module.encryption.rds-key.arn
+    rds_sg = module.networking.rds-sg.id
+}
+*/
+
+module "encryption" {
+    source = "./modules/encryption"
+    rds_supersecretpassword = module.data.rdspassword.result
+}
