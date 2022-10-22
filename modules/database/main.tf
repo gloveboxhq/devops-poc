2ae34ae7e27869f16022b3ab7a69ad3f7e4665d1
brooks <brooks.houck@gmail.com>
2022-10-22 19:05:18 +0000

    adding read replica config

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -1,13 +1,35 @@
 resource "aws_db_instance" "default" {
   allocated_storage    = 10
   db_name              = "challenge_db"
-  engine               = "postgresql"
+  identifier           = "challengedb"
+  engine               = "postgres"
   engine_version       = "14.1"
   instance_class       = "db.t3.micro"
-  username             = "rds-user98765"
-  password             = "temppassword"
+  username             = var.rds-root-user
+  password             = var.rds-secret
   skip_final_snapshot  = true
   iam_database_authentication_enabled = true
   kms_key_id = var.kms_key
+  storage_encrypted = true
   vpc_security_group_ids = [var.rds_sg]
+
+# Backups are required in order to create a replica
+  maintenance_window      = "Mon:00:00-Mon:03:00"
+  backup_window           = "03:00-06:00"
+  backup_retention_period = 1
+}
+
+resource "aws_db_instance" "challengedb_read" {
+  identifier             = "challengedbreader"
+  replicate_source_db    = aws_db_instance.default.identifier ## refer to the master instance
+  #db_name                = "challenge_db1"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 10
+  #engine                 = "postgres"
+  #engine_version         = "14.1"
+  skip_final_snapshot    = true
+  vpc_security_group_ids = [var.rds_sg]
+
+# disable backups to create DB faster
+  backup_retention_period = 0
 }
