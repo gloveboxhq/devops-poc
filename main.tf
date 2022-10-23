d987f189c596b5810ea6c1ddfc31c75964d14126
brooks <brooks.houck@gmail.com>
2022-10-23 02:21:47 +0000

    adding directory service config and acm cert to root module

--- main.tf
+++ main.tf
@@ -12,6 +12,9 @@ module "iam" {
     analyst_policy = module.data.rds_analyst_policy.arn
     admin_policy = module.data.admin_policy.arn
     read-replica-arn = module.database.rds_read_replica.arn
+    directory-secret = module.encryption.directory-password
+    vpc_id = module.data.vpc_data.id
+    subnet_ids = module.data.default_subnets.ids
 }
 
 
@@ -29,5 +32,13 @@ module "encryption" {
     source = "./modules/encryption"
     rds_supersecretpassword = module.data.rdspassword.result
     secret-id = module.data.random_integer.result
+    directory_supersecretpassword = module.data.directory_password.result
+    directory_domain = var.directory-domain-name
+}
 
+# for importing the created acm certificate
+resource "aws_acm_certificate" "cert" {
+    private_key = "unknown" 
+    certificate_body = "unknown" 
+    
 }
