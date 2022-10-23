bd216b47ac24522978f3efd35da6386c77028c74
brooks <brooks.houck@gmail.com>
2022-10-23 02:22:08 +0000

    adding outputs to test info

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,12 @@
 output "read-replica-arn" {
     value = module.database.rds_read_replica.arn
 }
+
+
+output "vpc_info" {
+    value = module.data.vpc_data
+}
+
+output "subnets" {
+    value = module.data.default_subnets.ids
+}
